36c02749720ed671eb8e99ac4e25cf18feced9d9
GerSay <kryt.master@yandex.ru>
2022-02-02 10:30:37 +0300

    memory usage of vector

--- libs/data_structures/vector/vector.c
+++ libs/data_structures/vector/vector.c
@@ -15,10 +15,30 @@ vector createVector(size_t n) {
 }
 
 void reserve(vector *v, size_t newCapacity) {
+    v->data = realloc(v->data, sizeof(int) * newCapacity);
+    if (v->data == NULL)
+        error();
     if (newCapacity == 0)
         v->data = NULL;
     if (newCapacity < v->size)
         v->size = newCapacity;
-    v->data = realloc(v->data, sizeof(int) * newCapacity);
 }
+
+void clear(vector *v) {
+    v->size = 0;
+}
+
+void shrinkToFit(vector *v) {
+    reserve (v, v->size);
+}
+
+void deleteVector(vector *v) {
+    free(v->data);
+    v->size = 0;
+    v->capacity = 0;
+}
+
+
+
+
 
