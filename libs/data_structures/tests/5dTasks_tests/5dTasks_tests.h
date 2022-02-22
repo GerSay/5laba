90482a6bf42a7ebe9579d453f373d53ec0bf89ce
GerSay <kryt.master@yandex.ru>
2022-02-23 01:28:09 +0300

    addInHeaderFile

--- libs/data_structures/tests/5dTasks_tests/5dTasks_tests.h
+++ libs/data_structures/tests/5dTasks_tests/5dTasks_tests.h
@@ -10,8 +10,51 @@ void test_swapRowsSquareMatrixWithMaxMinElement();
 //2
 void test_sortRowsByMinElement();
 
+//3
+void test_sortColsByMinElement();
 
+//4
+void test_getSquareOfMatrixIfSymmetric_SymmetricSquareMatrix();
 
+void test_getSquareOfMatrixIfSymmetric();
+
+//5
+void test_transposeIfMatrixHasEqualSumOfRows();
+
+//6
+
+//7
+void test_findSumOfMaxesOfPseudoDiagonal();
+
+//8
+void test_getMinInArea();
+
+//9git
+void test_sortByDistances();
+
+//10
+void test_countEqClassesByRowsSum();
+
+//11
+void test_getNSpecialElement();
+
+//12
+void test_swapPenultimateRow();
+
+//13
+void test_countNonDescendingRowsMatrices();
+
+//14
+void test_countZeroRows();
+
+//15
+void test_getMatrixNorm();
+
+//16
+
+//17
+
+//18
 
 
 
