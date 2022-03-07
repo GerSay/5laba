b6aef3305006f1fdba6ad5c04ac3819d91c4f3f0
GerSay <kryt.master@yandex.ru>
2022-03-07 16:35:45 +0300

    rewerseWords

--- libs/data_structures/tests/5eTasks_tests/5eTasks_tests.c
+++ libs/data_structures/tests/5eTasks_tests/5eTasks_tests.c
@@ -244,6 +244,39 @@ void test_isWordsOrdered() {
 
 //----------------------------------------7-----------------------------------------------
 
+void test_reverseWords_moreLettersInWord() {
+    char s[MAX_STRING_SIZE] = "this   task   has   caused me   a lot of pain ";
+    reverseWords(s);
 
+    ASSERT_STRING("pain of lot a me caused has task this", s);
+}
+
+void test_reverseWords_oneWord() {
+    char s[MAX_STRING_SIZE] = "pain";
+    reverseWords(s);
+
+    ASSERT_STRING("pain", s);
+}
+
+void test_reverseWords_oneLetter() {
+    char s[MAX_STRING_SIZE] = "a b c a b c";
+    reverseWords(s);
+
+    ASSERT_STRING("c b a c b a", s);
+}
+
+void test_reverseWords_empty() {
+    char s[MAX_STRING_SIZE] = "";
+    reverseWords(s);
+
+    ASSERT_STRING("", s);
+}
+
+void test_reverseWords() {
+    test_reverseWords_moreLettersInWord();
+    test_reverseWords_oneLetter();
+    test_reverseWords_empty();
+    test_reverseWords_oneWord();
+}
 
 //----------------------------------------8-----------------------------------------------
--- libs/tasks/5eTasks/5eTasks.c
+++ libs/tasks/5eTasks/5eTasks.c
@@ -113,7 +113,24 @@ bool isWordsOrdered(char *s) {
 
 //----------------------------------------7-----------------------------------------------
 
+void reverseWords(char *s) {
+    char *end = copy(s, getEndOfString(s), _stringBuffer);
+    char *beginCopy = s;
+    char *beginSearch = end - 1;
+    wordDescriptor w;
+    while (getWordReverse(beginSearch, _stringBuffer - 1, &w)) {
+        beginCopy = copy(w.begin, w.end, beginCopy);
+        *beginCopy++ = ' ';
 
+        beginSearch -= w.end - w.begin + 1;
+        beginSearch = findNonSpaceReverse(beginSearch, _stringBuffer - 1);
+    }
+
+    if (beginCopy != s)
+        beginCopy--;
+
+    *beginCopy = '\0';
+}
 
 //----------------------------------------8-----------------------------------------------
 
