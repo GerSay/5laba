e3c54abfdd888bbed8a14364458600ae26e58d34
GerSay <kryt.master@yandex.ru>
2022-03-13 12:36:57 +0300

    add-sorts

--- main.c
+++ main.c
@@ -3,25 +3,21 @@
 #include <stdbool.h>
 #include <time.h>
 
-
-/*
-#define typeArr (a) \
-if (a == 1)
-return char;
-if (a == 2)
-    return short int;
-if (a == 4)
-    return int;
-*/
+#include "libs/algorithms/array/array.h"
+#include "libs/algorithms/base/base.h"
 
 
-
-#define TIME_TEST/*({  \
-sortFunc(innerBuffer, size); \
-}, time);            \*/
+#define TIME_TEST(testCode, time) {             \
+    clock_t start_time = clock();               \
+    testCode                                    \
+        clock_t end_time = clock();             \
+    clock_t sort_time = end_time - start_time;  \
+    time = (double) sort_time / CLOCKS_PER_SEC; \
+}
 
 #define ARRAY_SIZE(a) sizeof(a) / sizeof(a[0])
 
+#define DECREASE_FACTOR 1.24733
 
 typedef struct sortFunc {
     void (*sort) (int *a, size_t n);
@@ -42,6 +38,7 @@ double getTime() {
     return (double) sort_time / CLOCKS_PER_SEC;
 }
 
+
 void checkTime(void (*sortFunc) (int *, size_t),
                void (*generateFunc) (int *, size_t),
                size_t size, char *experimentName) {
@@ -60,9 +57,80 @@ void checkTime(void (*sortFunc) (int *, size_t),
               }, time);
 }
 
-void selectionSort(typeArr *a, size_t n) {
 
+
+///////////////////////////////////////////////////////////////////////
+void exchangeSort(int *a, size_t n) {
+    int tmp;
+    bool noSwap;
+
+    for (size_t i = n; i > 0; i--) {
+        noSwap = 1;
+        for (size_t j = 0; j + 1 < i; j++) {
+            if (a[j] > a[j + 1])
+                tmp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = tmp;
+                noSwap = 0;
+            }
+        if (noSwap == 1)
+            break;
+    }
 }
+
+void selectionSort(int *a, size_t n) {
+    for (size_t i = 0; i < n; i++) {
+        size_t minPos = i;
+        for (size_t j = i + 1; j < n; j++)
+            if (a[minPos] > a[j])
+                minPos = j;
+        int tmp = a[minPos];
+        a[minPos] = a[i];
+        a[i] = tmp;
+    }
+}
+
+void insertionSort(int *a, size_t n) {
+    int newElement,
+    size_t location;
+    for (size_t i = 1; i < n; i++) {
+        newElement = a[i];
+        location = i - 1;
+        while(location >= 0 && a[location] > newElement) {
+            a[location + 1] = a[location];
+            location--;
+        }
+        a[location + 1] = newElement;
+    }
+}
+
+void combSort(int *a, size_t n) {
+    size_t t = n - 1;
+    while (t > 0) {
+        for (size_t i = 0, j = t; j < n; i++, j++)
+            if (a[i] > a[j])
+                swapU(&a[i], &a[j], sizeof(a));
+        t /= DECREASE_FACTOR;
+    }
+}
+
+void shellSort(int *a, size_t n) {
+    int tmp;
+    for (size_t step = n / 2; step > 0; step /= 2)
+        for (size_t i = step; i < n; i++) {
+            tmp = a[i];
+            size_t j = i;
+            for (; j >= step; j -= step) {
+                if (tmp < a[j - step])
+                    a[j] = a[j - step];
+                else
+                    break;
+            }
+            a[j] = tmp;
+        }
+}
+
+//////////////////////////////////////////////////////////////////////////
 
 void timeExperiment() {
     //описание функций сортировки
@@ -100,10 +168,31 @@ void timeExperiment() {
         }
         printf("\n");
     }
+
+    printf("Status: ");
+    if (isOrdered(innerBuffer, size)) {
+        printf("OK! Time: %.3f s.\n", time);
+
+        char filename[256];
+        sprintf(filename, "./data%s.csv", name);
+        FILE *f = fopen(filename, "a");
+        if (f == NULL) {
+            printf("FileOpenError %s", filename);
+            exit(1);
+        }
+        fprintf(f, "%zu; %.3f\n", size, time);
+        fclose(f);
+    } else {
+        printf("Wrong!\n");
+
+        outputArray_(innerBuffer, size);
+
+        exit(1);
+    }
 }
 
 int main() {
-    timeExperinment();
+    timeExperiment();
 
     return 0;
 }
